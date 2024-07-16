06fc37ea0bcfa383e7a2f07740c2ef68139ffe29
CodeConnoisseur74 <sherry.andrews.com>
2024-07-17 00:42:48 +0200

    setup url pattertns in note/views.py

--- note/urls.py
+++ note/urls.py
@@ -1,1 +1,10 @@
 from django.urls import path
+
+from note import views
+
+urlpatterns = [
+    path('', views.homepage, name=''),
+    path('register', views.register, name='register'),
+    path('my-login', views.my_login, name='my-login'),
+    path('dashboard', views.dashboard, name='dashboard'),
+]
