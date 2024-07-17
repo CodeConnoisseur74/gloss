09d66c8c1fc7aedcdc58683dad3c6b1dee6a3b3f
CodeConnoisseur74 <sherry.andrews.com>
2024-07-17 17:47:19 +0200

    set debug to false for early deployment

--- gloss/settings.py
+++ gloss/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)w2=r6^#$4aa(oq0a4=uy%ka2l!$k72qw2ly853og%**pja%uc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1']
 
