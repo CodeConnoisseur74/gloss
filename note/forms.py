960ca139e9062cd0d331c080f0fd172c5e4249eb
CodeConnoisseur74 <sherry.andrews.com>
2024-07-22 10:47:07 +0200

    Add dashboard and create thought functionality to navbar, urls and views

--- note/forms.py
+++ note/forms.py
@@ -3,7 +3,17 @@ from typing import ClassVar
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 from django.forms.widgets import PasswordInput, TextInput
+
+from note.models import Note
+
+
+class NoteForm(ModelForm):
+    class Meta:
+        model = Note
+        fields: ClassVar[list[str]] = ['title', 'content']
+        exclude: ClassVar[list[str]] = ['user']
 
 
 class CreateUserForm(UserCreationForm):
--- note/urls.py
+++ note/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('my-login', views.my_login, name='my-login'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('user-logout', views.user_logout, name='user-logout'),
+    path('create-note', views.create_note, name='create-note'),
 ]
